44f3fd312525224ec080cbd38788be7945aa6c63
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-07 20:03:52 +0700

    feat: fix height main

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import { ModeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="p-10 h-full w-full overflow-hidden">
-      <div className="bg-white/10 backdrop-blur rounded-lg w-full h-full p-4">
-        <ModeToggle />
+    <main className="p-10 h-screen w-full overflow-hidden flex items-center justify-center">
+      <div className="border-2 border-white/10 backdrop-blur rounded-lg w-full h-full p-4">
+        <div className="w-full right-0 flex justify-end">
+          <ModeToggle />
+        </div>
       </div>
     </main>
   );
