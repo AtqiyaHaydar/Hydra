98ee14665ec6fe9a29115ab4d5a84186eeb9b285
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-09 00:49:00 +0700

    feat: button interface

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Link from 'next/link'
 import { Button } from './ui/button'
 import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 interface NavItemI {
   title: string
@@ -14,7 +15,16 @@ const NavItem = ({ title, href }: NavItemI) => {
   const pathname = usePathname()
   
   return (
-    <Button variant={pathname === href ? "secondary" : "ghost"} className='text-center w-[200px]'>
+    <Button 
+      variant={pathname === href ? "default" : "ghost"} 
+      className={cn(
+        pathname === "/" && pathname === href && 'bg-gradient-to-r gradient-1',
+        pathname === "/tasks" && pathname === href && 'bg-gradient-to-r gradient-2',
+        pathname === "/projects" && pathname === href && 'bg-gradient-to-r gradient-8',
+        pathname === href && "text-white font-bold",
+        'text-center w-[200px]'
+      )}
+    >
       <Link href={href}>
         {title}
       </Link>
