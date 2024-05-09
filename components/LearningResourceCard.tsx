50d69513957e30bdf1d6a9022f066b77850a723b
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-09 21:32:18 +0700

    style: learning button

--- components/LearningResourceCard.tsx
+++ components/LearningResourceCard.tsx
@@ -16,7 +16,7 @@ const LearningResourceCard = ({ title, link, image, className } : LearningResour
   return (
     <Link href={link} target='_blank'>
       <Card className={cn('bg-gradient-to-r transition-all min-w-[100px] h-[50px] flex items-center justify-center hover:bg-opacity-75  p-[1px]', className)}>
-        <div className='w-full h-full bg-dark/90 px-4 hover:bg-dark/10 flex items-center justify-center rounded-md transition-all'>
+        <div className='w-full h-full bg-dark/95 px-4 hover:bg-dark/10 flex items-center justify-center rounded-md transition-all'>
           <h1 className='font-medium text-sm text-center'>{title}</h1>
         </div>
       </Card>
