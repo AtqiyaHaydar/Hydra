39b6448760ed5e318fcdd4b2efafb26e346c622d
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-09 15:04:01 +0700

    feat: fix button navigation trigger

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -15,20 +15,20 @@ const NavItem = ({ title, href }: NavItemI) => {
   const pathname = usePathname()
   
   return (
-    <Button 
-      variant={pathname === href ? "default" : "ghost"} 
-      className={cn(
-        pathname === "/" && pathname === href && 'bg-gradient-to-r gradient-1',
-        pathname === "/tasks" && pathname === href && 'bg-gradient-to-r gradient-2',
-        pathname === "/projects" && pathname === href && 'bg-gradient-to-r gradient-8',
-        pathname === href && "text-white font-bold",
-        'text-center w-[200px] transition-all'
-      )}
-    >
-      <Link href={href}>
+    <Link href={href}>
+      <Button 
+        variant={pathname === href ? "default" : "ghost"} 
+        className={cn(
+          pathname === "/" && pathname === href && 'bg-gradient-to-r gradient-1',
+          pathname === "/tasks" && pathname === href && 'bg-gradient-to-r gradient-2',
+          pathname === "/projects" && pathname === href && 'bg-gradient-to-r gradient-8',
+          pathname === href && "text-white font-bold",
+          'text-center w-[200px] transition-all'
+        )}
+      >
         {title}
-      </Link>
-    </Button>
+      </Button>
+    </Link>
   )
 }
 
